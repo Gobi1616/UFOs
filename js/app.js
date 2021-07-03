bef61d1f6795ea2ae55450b00a5a0f741c112f8b
Gobinath Thangaiya (GOBI) <tgobinath1986@gmail.com>
2021-07-03 11:52:38 -0400

    Updated_app.js

--- js/app.js
+++ js/app.js
@@ -19,10 +19,9 @@ function buildTable(data) {
       Object.values(dataRow).forEach((val) => {
         let cell = row.append("td");
         cell.text(val);
-        }
-      );
+      });
     });
-}
+  }
 
 function handleClick() {
     // Grab the datetime value from the filter
